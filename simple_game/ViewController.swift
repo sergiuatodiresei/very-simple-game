c8e6ba844ff801b7b5f53eab25101bcf52ffe98d
Sergiu Atodiresei <sergiu_atodiresei@macbook-pro-sergiu.local>
2016-08-19 15:42:41 +0300

    Initial Commit

---
+++ simple_game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simple_game
+//
+//  Created by Sergiu Atodiresei on 19.08.2016.
+//  Copyright © 2016 SergiuApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
